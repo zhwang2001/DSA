14addb839b3b96ffd2376e2784614c0993d8b131
Zihao Wang <zhwang2001@gmail.com>
2022-09-29 23:26:20 -0400

    added question 2215 Find the difference of two arrays

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -3,6 +3,41 @@
 #implement pseudo code
 #implement real code
 
+#Find the Difference of Two Arrays question 2215
+class Solution(object):
+    """
+    Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+    answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+    answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+    Note that the integers in the lists may be returned in any order.   
+    """
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        
+        Explanation:
+
+        if im subtracting nums2 from nums1, it will immedialty discard
+        all values from nums2 and discard duplicate values that appeared in both nums1 and nums2
+        """
+
+        return [list(set(nums1) - set(nums2)), list(set(nums2)-set(nums1))]
+
+
+        
+sol = Solution()
+print("--Find Difference between 2 arrays--")
+print(sol.findDifference([1,2,3,4], [2,3,4,5])) #returns [[1],[5]]
+print(sol.findDifference([2,5,6,7], [1,2,7,6])) #returns [[5], [1]]
+print('\n\n\n')
+
+
+exit()
+
+
 #Valid Anagram question 242
 class Solution(object):
     """
@@ -140,8 +175,6 @@ sol.rotate([1,2], 3)
 #sol.rotate([2,3,4,5,1,2,3,4,1,2,3,1,2,3,1,2,3,1,3,1,12,12,12,12,12,141,1515,15], 92000)
 print('\n\n\n')
 
-
-exit()
 
 #Find Duplicate Number question 287
 class Solution:
