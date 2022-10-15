931b861c9d029f6ab6fbe57df6d3ccd143515d8c
Zihao Wang <zhwang2001@gmail.com>
2022-10-14 20:49:57 -0400

    added check if wrod has prefix question

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -5,6 +5,40 @@
 
 
 
+#Counting words with a given prefix question 2185
+class Solution(object):
+    """
+    You are given an array of strings words and a string pref.
+    Return the number of strings in words that contain pref as a prefix.
+    A prefix of a string s is any leading contiguous substring of s.
+
+    Input: words = ["pay","attention","practice","attend"], pref = "at"
+    Output: 2
+    Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+    """
+    def prefixCount(self, words, pref):
+        """
+        :type words: List[str]
+        :type pref: str
+        :rtype: int
+        
+        O(N) time complexity
+        O(1) space complexity
+        """
+        count = 0
+        length = len(pref)
+        for word in words:
+            if pref in word[:length]:
+                count += 1
+        return count
+
+
+sol = Solution()
+print('--Counting words with a given prefix--')
+print(sol.prefixCount(["leetcode","win","loops","attend"], pref = "code"))
+print(sol.prefixCount(["pay","attention","atmosphere","fachoi"], pref= "at"))
+print('\n\n\n')
+
 #Finbonaci number question 509
 class Solution(object):
     """
